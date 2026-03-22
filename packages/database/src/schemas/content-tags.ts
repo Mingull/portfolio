878609c29783e6c1c29e2228337804e73e4d4e9b
Mingull <60094450+Mingull@users.noreply.github.com>
2026-03-22 22:03:03 +0100

    Drizzle mysql migrations (#11)
    * Drizzle mysql migrations
    
    * Possible fix for CI
    
    * Updated migrations
    
    * Updated migrations again and added the updated schemas

--- packages/database/src/schemas/content-tags.ts
+++ packages/database/src/schemas/content-tags.ts
@@ -1,11 +1,11 @@
-import { mysqlTable, varchar, primaryKey, char } from "drizzle-orm/mysql-core";
+import { mysqlTable, primaryKey, varchar } from "drizzle-orm/mysql-core";
 import { content } from "./content";
 import { tags } from "./tags";
 
 export const contentTags = mysqlTable(
 	"content_tags",
 	{
-		contentId: char("content_id", { length: 36 })
+		contentId: varchar("content_id", { length: 36 })
 			.notNull()
 			.references(() => content.id),
 		tagId: varchar("tag_id", { length: 36 })
