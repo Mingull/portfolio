21b0b9c79494871149ef3fcecd91d037d2abdd59
Mingull <nielsplug@outlook.com>
2026-03-27 10:46:17 +0100

    Fixed inject

--- apps/api/src/lib/inject.ts
+++ apps/api/src/lib/inject.ts
@@ -1,8 +1,8 @@
-import { createPostRepository } from "@/features/posts/repository";
+import { createPostRepository } from "@/features/content/posts/repository";
+import { createPostService } from "@/features/content/posts/service";
 import { createProjectRepository } from "@/features/projects/repository";
-import { createSkillRepository } from "@/features/skills/repository";
-import { createPostService } from "@/features/posts/service";
 import { createProjectService } from "@/features/projects/service";
+import { createSkillRepository } from "@/features/skills/repository";
 import { createSkillService } from "@/features/skills/service";
 import { db } from "@mingull/database/client";
 import { createInjector } from "@mingull/injector";
