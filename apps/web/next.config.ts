dd1ad0d6d35e449c8e6882df6cf2c1d34c4acc2a
Mingull <nielsplug@outlook.com>
2026-04-03 13:04:07 +0200

    Removed duplicated image remote patterns

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -9,12 +9,6 @@ const nextConfig: NextConfig = {
 		unoptimized: process.env.NODE_ENV !== "production", // Disable optimization in development for faster builds
 		remotePatterns: [
 			new URL(process.env.BASE_API_URL ? `${process.env.BASE_API_URL}/**` : "http://localhost:3001/**"), // Allow all images from the API URL
-			{
-				protocol: process.env.NODE_ENV === "production" ? "https" : "http",
-				hostname: process.env.NODE_ENV === "production" ? "api.mingull.nl" : "localhost",
-				port: process.env.NODE_ENV === "production" ? undefined : "3001",
-				pathname: "/**",
-			},
 		],
 	},
 	reactStrictMode: true,
